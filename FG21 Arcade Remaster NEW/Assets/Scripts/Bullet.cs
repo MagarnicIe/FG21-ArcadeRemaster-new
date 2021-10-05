3a846c0899eee0ee2abd649f66eed70e13a04865
Kallegan <karl.berglund@futuregames.nu>
2021-10-05 14:38:52 +0200

    Melee script.

--- FG21 Arcade Remaster NEW/Assets/Scripts/Bullet.cs
+++ FG21 Arcade Remaster NEW/Assets/Scripts/Bullet.cs
@@ -25,8 +25,6 @@ public class Bullet : MonoBehaviour
             Instantiate(impactEffect, transform.position, transform.rotation);
         }
 
-        
-        
         Destroy(gameObject);
     }
 }
