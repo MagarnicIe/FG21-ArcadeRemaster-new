cc15fa66e7c3af8558c988562e78ecbfbdafd189
MagarnicIe <90769104+MagarnicIe@users.noreply.github.com>
2021-10-07 11:31:57 +0200

    traps are tilesets

--- FG21 Arcade Remaster NEW/Assets/Scripts/Traps.cs
+++ FG21 Arcade Remaster NEW/Assets/Scripts/Traps.cs
@@ -1,14 +1,15 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using UnityEngine.Tilemaps;
 using UnityEngine;
-[RequireComponent(typeof(BoxCollider2D))]
+[RequireComponent(typeof(TilemapCollider2D))]
 public class Traps : MonoBehaviour
 {
     
     private void Reset()
     {
-        GetComponent<BoxCollider2D>().isTrigger = true;
+        GetComponent<TilemapCollider2D>().isTrigger = true;
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
