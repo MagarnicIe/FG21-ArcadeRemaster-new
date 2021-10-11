e614cb39c126e003380b5762fc672efb7de72757
DPayne95 <90388559+DPayne95@users.noreply.github.com>
2021-10-11 17:14:07 +0200

    HealthSystem

---
+++ FG21 Arcade Remaster NEW/Assets/Scripts/Health.cs
@@ -0,0 +1,71 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Health : MonoBehaviour
+
+{
+    public int health;
+    public int numOfHearts;
+
+    public Image[] hearts;
+    public Sprite fullHeart;
+    public Sprite emptyHeart;
+
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+
+        if (collision.collider.tag == "Enemy")
+        {
+            health -= 1;
+        }
+
+    }
+    void Update()
+    {
+        
+
+        if (health > numOfHearts)
+        {
+            health = numOfHearts;
+
+        }
+
+        for (int i = 0; i < hearts.Length; i++)
+        {
+            if (i < health)
+            {
+                hearts[i].sprite = fullHeart;
+
+            }
+            else
+            {
+                hearts[i].sprite = emptyHeart;
+
+            }
+            if (i < numOfHearts)
+            {
+                hearts[i].enabled = true;
+
+            }
+            else
+            {
+
+                hearts[i].enabled = false;
+
+
+            }
+                    
+                
+
+            
+             
+        }
+
+        
+
+
+    }
+}
