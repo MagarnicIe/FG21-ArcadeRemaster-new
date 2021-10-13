b7d60e4f5a65c114cf8fbd222bfc45c5cbc2d986
MagarnicIe <90769104+MagarnicIe@users.noreply.github.com>
2021-10-13 15:38:34 +0200

    Revert "MainMenu"
    This reverts commit 074b374a8bc96523784fc40c7c82c85f8aa11611.

--- FG21 Arcade Remaster NEW/Assets/Scripts/AIFOLLOWTEST.cs
+++ FG21 Arcade Remaster NEW/Assets/Scripts/AIFOLLOWTEST.cs
@@ -9,13 +9,10 @@ public class AIFOLLOWTEST : MonoBehaviour
     public Transform player;
     public float lineOfSite;
     public Animator animator;
-    private Flip flip;
-    Rigidbody2D  rb;
     private void Start()
     {
         player = GameObject.FindGameObjectWithTag("Player").transform;
-        rb = animator.GetComponent<Rigidbody2D>();
-        flip = animator.GetComponent<Flip>();
+        animator = GetComponent<Animator>();
     }
 
     // Update is called once per frame
@@ -39,8 +36,6 @@ public class AIFOLLOWTEST : MonoBehaviour
         float distanceFromPlayer = Vector2.Distance(player.position, transform.position);
         if(distanceFromPlayer < lineOfSite)
         {
-            animator.GetComponent<Flip>().LookAtPlayer();
-            
             animator.SetBool("moving", true);
             transform.position = Vector2.MoveTowards(this.transform.position, player.position, speed * Time.deltaTime);
             
