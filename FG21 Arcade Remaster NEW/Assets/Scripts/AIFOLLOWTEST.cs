edb1d244af816bf9c77e8788ba08c6bfb4e08c2a
MagarnicIe <90769104+MagarnicIe@users.noreply.github.com>
2021-10-13 15:02:20 +0200

    they do the flip yes

--- FG21 Arcade Remaster NEW/Assets/Scripts/AIFOLLOWTEST.cs
+++ FG21 Arcade Remaster NEW/Assets/Scripts/AIFOLLOWTEST.cs
@@ -9,42 +9,47 @@ public class AIFOLLOWTEST : MonoBehaviour
     public Transform player;
     public float lineOfSite;
     public Animator animator;
+    private Flip flip;
+    Rigidbody2D rb;
     private void Start()
     {
         player = GameObject.FindGameObjectWithTag("Player").transform;
-        animator = GetComponent<Animator>();
+        rb = animator.GetComponent<Rigidbody2D>();
+        flip = animator.GetComponent<Flip>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        if ( player != null)
+        if (player != null)
         {
             CheckForPlayer();
         }
-       
+
     }
 
     private void OnDrawGizmos()
     {
         Gizmos.color = Color.green;
-        Gizmos.DrawWireSphere(transform.position,lineOfSite);
+        Gizmos.DrawWireSphere(transform.position, lineOfSite);
     }
 
     private void CheckForPlayer()
     {
         float distanceFromPlayer = Vector2.Distance(player.position, transform.position);
-        if(distanceFromPlayer < lineOfSite)
+        if (distanceFromPlayer < lineOfSite)
         {
+            animator.GetComponent<Flip>().LookAtPlayer();
+
             animator.SetBool("moving", true);
             transform.position = Vector2.MoveTowards(this.transform.position, player.position, speed * Time.deltaTime);
-            
+
         }
         else
         {
             animator.SetBool("moving", false);
         }
     }
-    
-    
+
+
 }
