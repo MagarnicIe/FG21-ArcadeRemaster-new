abd720a00ca727d25765f2a6d25eac506e91f3a5
DPayne95 <90388559+DPayne95@users.noreply.github.com>
2021-10-14 11:43:21 +0200

    Mainmenu

--- FG21 Arcade Remaster NEW/Assets/Scripts/MainMenu.cs
+++ FG21 Arcade Remaster NEW/Assets/Scripts/MainMenu.cs
@@ -11,10 +11,10 @@ public class MainMenu : MonoBehaviour
       SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1); //will load the next scene in queue.
    }
 
-   public void ExitGame()
+   public void doExitGame()
    {
       Application.Quit(); //will shut down the game when built.
-      Debug.Log("Exiting game..."); //temp debug to test menu exiting function. Should be deleted before final build.
+       
    }
    
    public void RestartGame()
